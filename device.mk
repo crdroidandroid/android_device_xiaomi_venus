cca9e748ddbe4bbe136512e6038d2fecc354a3ef
Erfan Abdi <erfangplus@gmail.com>
2021-12-18 07:37:10 +0330

    mars: Add hotword priv app permissions
    Change-Id: Ic936e852cb77fe47c0ce69465b7a3717c2619b7e

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@ PRODUCT_COPY_FILES += \
 # Fingerprint
 TARGET_HAS_UDFPS := true
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
