e923d1f6f00ce9097ec03e1ecac2e854ea3e5612
FlowerSea0208 <zzp060208@gmail.com>
2022-07-31 12:13:49 +0800

    venus: crDroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_venus.mk
+    $(LOCAL_DIR)/lineage_venus.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_venus-user \
-    aosp_venus-userdebug \
-    aosp_venus-eng
+    lineage_venus-user \
+    lineage_venus-userdebug \
+    lineage_venus-eng
--- lineage_venus.mk
+++ lineage_venus.mk
@@ -11,16 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from venus device
 $(call inherit-product, device/xiaomi/venus/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := venus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2011K2G
-PRODUCT_NAME := aosp_venus
+PRODUCT_NAME := lineage_venus
 
 TARGET_BOOT_ANIMATION_RES := 1440
 
